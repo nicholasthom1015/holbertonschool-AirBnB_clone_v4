8adbcabc1fab7e92388763c1965af6a9c2380408
root <nicholasthomas10.15@gmail.com>
2023-04-02 18:21:09 +0000

    doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
+
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
 from flask_cors import CORS, cross_origin
